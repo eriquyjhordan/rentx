1e9559c210cb1907ab596925c511996d11fc704e
ERIQUY JHORDAN <eriquy@gmail.com>
2024-01-29 12:26:45 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import Home from './src/screens/Home'
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <StatusBar style="light" />
+    </>
+  )
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="flex-1 justify-center items-center bg-zinc-800">
+            <Text className="text-white text-4xl">Hello World</Text>
+        </View>
+    )
+
+}
